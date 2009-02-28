f537fea7d6fba1ba7f1f0f1a22cc2f5220c7fc24
Valentin Nemcev <valentine@168.166.249.ozerki.net>
2009-02-28 17:05:55 +0300

    Revert "Generated authentication" Fuck that, I'll do it later Also, tested git revert
    This reverts commit 5aef856af57f8708443aefe125c3a7de1e0c1d1c.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'admins', :action => 'create'
-  map.signup '/signup', :controller => 'admins', :action => 'new'
-  map.resources :admins
-
-  map.resource :session
-
   map.resources :articles
 
   map.resources :categories
