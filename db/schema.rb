a0eb0aa2a7e36fd85ff2c6f85dcfd0fce9fb3149
Valentin Nemcev <valentine@168.166.249.ozerki.net>
2009-03-01 23:58:03 +0300

    Добавлено поле "текст" для статей

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090228113206) do
+ActiveRecord::Schema.define(:version => 20090301204406) do
 
   create_table "articles", :force => true do |t|
     t.integer  "category_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20090228113206) do
     t.string   "link"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "text"
   end
 
   create_table "categories", :force => true do |t|
