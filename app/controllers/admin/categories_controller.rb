c5f69638ad2a8e281d2ad699b4d5bc60d37767b4
Valentin Nemcev <valentine@168.166.249.ozerki.net>
2009-03-01 22:48:20 +0300

    Cleaned up categories controller Changed category select

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,86 +1,48 @@
 class Admin::CategoriesController < ApplicationController
   layout "admin"
-  # GET /categories
-  # GET /categories.xml
+  
+  
   def index
     @categories = Category.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @categories }
-    end
+    render :action => "index"
   end
 
-  # GET /categories/1
-  # GET /categories/1.xml
-  def show
-    @category = Category.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @category }
-    end
-  end
-
-  # GET /categories/new
-  # GET /categories/new.xml
   def new
     @category = Category.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @category }
-    end
+    render :action => "edit" #Заменить на что-нибудь более логичное
   end
 
-  # GET /categories/1/edit
   def edit
     @category = Category.find(params[:id])
+    render :action => "edit"
   end
 
-  # POST /categories
-  # POST /categories.xml
   def create
     @category = Category.new(params[:category])
 
-    respond_to do |format|
-      if @category.save
-        flash[:notice] = 'Category was successfully created.'
-        format.html { redirect_to(@category) }
-        format.xml  { render :xml => @category, :status => :created, :location => @category }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @category.errors, :status => :unprocessable_entity }
-      end
+    if @category.save
+      flash[:notice] = 'Category was successfully created.'
+      redirect_to admin_categories_url
+    else
+      render :action => "new" 
     end
   end
 
-  # PUT /categories/1
-  # PUT /categories/1.xml
   def update
     @category = Category.find(params[:id])
 
-    respond_to do |format|
-      if @category.update_attributes(params[:category])
-        flash[:notice] = 'Category was successfully updated.'
-        format.html { redirect_to(@category) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @category.errors, :status => :unprocessable_entity }
-      end
+    if @category.update_attributes(params[:category])
+      flash[:notice] = 'Category was successfully updated.'
+      redirect_to admin_categories_url
+    else
+      render :action => "edit" 
     end
   end
 
-  # DELETE /categories/1
-  # DELETE /categories/1.xml
   def destroy
     @category = Category.find(params[:id])
     @category.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(categories_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to admin_categories_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.namespace :admin do |admin|
-    admin.resources :articles
-    admin.resources :categories
+    admin.resources :articles, :except => [:show] 
+    admin.resources :categories, :except => [:show]
   end  
   
 end
