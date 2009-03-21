0c65819da489613364b70b9a710133452c67cddf
Valentin Nemcev <valentine@168.166.249.ozerki.net>
2009-03-21 23:29:47 +0300

    Поправил ошибку с RedCloth

--- config/environment.rb
+++ config/environment.rb
@@ -68,7 +68,7 @@ Rails::Initializer.run do |config|
   # This is necessary if your schema can't be completely dumped by the schema dumper,
   # like if you have constraints or database-specific column types
   # config.active_record.schema_format = :sql
-
+  config.gem 'RedCloth'
   # Activate observers that should always be running
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
