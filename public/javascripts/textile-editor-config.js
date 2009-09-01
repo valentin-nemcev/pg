0bb86d41563fc2f10c022e1f6dc8657ed1e0e151
Valentine <valentine@Valentin.local>
2009-09-02 02:11:48 +0400

    Изменил страницу логина

--- public/javascripts/textile-editor-config.js
+++ public/javascripts/textile-editor-config.js
@@ -8,9 +8,9 @@ teButtons.push(new TextileEditorButton('ed_strike', 	    'strikethrough.png', '-
 // teButtons.push(new TextileEditorButton('ed_ul',				'list_bullets.png',  ' * ', '\n', '.', 'Bulleted List'));
 teButtons.push(new TextileEditorButton('ed_p',				'paragraph.png',     'p',   '\n', 'p', 'Paragraph'));
 // teButtons.push(new TextileEditorButton('ed_h1',				'h1.png',            'h1',  '\n', '1', 'Header 1'));
-teButtons.push(new TextileEditorButton('ed_h2',				'h2.png',            'h2',  '\n', '2', 'Header 2'));
+// teButtons.push(new TextileEditorButton('ed_h2',				'h2.png',            'h2',  '\n', '2', 'Header 2'));
 teButtons.push(new TextileEditorButton('ed_h3',				'h3.png',            'h3',  '\n', '3', 'Header 3'));
-// teButtons.push(new TextileEditorButton('ed_h4',				'h4.png',            'h4',  '\n', '4', 'Header 4'));
+teButtons.push(new TextileEditorButton('ed_h4',				'h4.png',            'h4',  '\n', '4', 'Header 4'));
 teButtons.push(new TextileEditorButton('ed_block',   		'blockquote.png',    'bq',  '\n', 'q', 'Blockquote'));
 // teButtons.push(new TextileEditorButton('ed_outdent', 		'outdent.png',       ')',   '\n', ']', 'Outdent'));
 // teButtons.push(new TextileEditorButton('ed_indent',  		'indent.png',        '(',   '\n', '[', 'Indent'));
