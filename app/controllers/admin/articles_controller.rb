349c38c432ab0a5034c5885935c76acf1a93cf4b
Valentine <valentine@Valentin.local>
2009-09-05 15:27:09 +0400

    Почистил код

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -5,21 +5,10 @@ class Admin::ArticlesController < AdminController
     render :action => "index"
   end
 
-  # def new
-  #     @revision = Revision.new
-  #     @form_method = :put
-  #     @form_url = new_admin_article_path()
-  #     render  "admin/revisions/edit"
-  #   end
-
-
   def new
     @article = Article.new
-    # @form_method = :put
-    #     @form_url = new_admin_article_path()
     render :action => "edit"
   end
-
 
   def edit
     @article = Article.find(params[:id])
@@ -33,7 +22,7 @@ class Admin::ArticlesController < AdminController
       flash[:notice] = 'Статья сохранена'
       redirect_to admin_articles_path
     else
-      render :action => "articles/edit"
+      render :action => "edit"
     end
   end
   
@@ -51,7 +40,6 @@ class Admin::ArticlesController < AdminController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    
     flash[:notice] = 'Статья удалена'
     redirect_to admin_articles_path
   end
--- app/controllers/admin/revisions_controller.rb
+++ app/controllers/admin/revisions_controller.rb
@@ -5,50 +5,6 @@ class Admin::RevisionsController < AdminController
     render :action => "index"
   end
 
-  def new
-    if params[:article_id].nil?
-      @revision = Revision.new
-    else
-      @revision = Article.find(params[:article_id]).current_revision.clone
-      @revision.article_id = params[:article_id] 
-    end
-    @revision.article.links.build
-    render :action => "edit"
-  end
-
-  def create
-    @revision = Revision.new(params[:revision])
-    @revision.editor = current_user
-    @revision.build_article if @revision.article.nil?
-    if @revision.save
-      @revision.article.current_revision = @revision
-      @revision.article.save
-      @revision.save(false)
-      flash[:notice] = 'Статья сохранена'
-      redirect_to admin_articles_path
-    else
-      render :action => "edit"
-    end
-
-  end
-  
-  def edit
-    @revision = Revision.find(params[:id])
-    render :action => "edit"
-  end
-  
-  def update
-    @revision = Revision.find(params[:id])
-
-    if @revision.update_attributes(params[:revision])
-      flash[:notice] = 'Статья сохранена'
-      redirect_to admin_articles_path
-    else
-      render :action => "edit"
-    end
-
-  end
-
   def destroy
     @revision = Revision.find(params[:id])
     @revision.destroy
