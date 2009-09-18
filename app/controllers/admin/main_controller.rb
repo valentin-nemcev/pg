04ec0feb78ccb0cf50d5b0c537d153cd16b36f1c
Valentine <valentine@Valentines-Mac-Pro.local>
2009-09-19 03:03:43 +0400

    Переделал главную страницу админки

--- app/controllers/admin/main_controller.rb
+++ app/controllers/admin/main_controller.rb
@@ -1,8 +1,9 @@
 class Admin::MainController < AdminController
   def main
     @articles = Hash.new
+    @images = Hash.new
     @articles[:total] = Article.count
-  
+    @images[:total] = Image.count
   end
 
 end
