0874bfdfda82e1293f1c8138be4707a7ca5118ce
Valentine <valentine@Valentines-Mac-Pro.local>
2009-09-20 22:03:51 +0400

    Изменил навигацию в админке

---
+++ app/helpers/admin/navigation_helper.rb
@@ -0,0 +1,11 @@
+module Admin::NavigationHelper
+
+  def navigation_link(condition, name, options = {}, html_options = {}, &block)
+    if condition
+      content_tag :span, name, html_options
+    else
+      link_to(name, options, html_options)
+    end
+  end
+    
+end
