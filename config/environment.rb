c2cc66930c6d4b80fc06e2c01757d1993e24ab3e
Valentine <valentine@mainframe.local>
2009-11-13 00:19:33 +0300

    Исправил ошибки

--- config/environment.rb
+++ config/environment.rb
@@ -39,10 +39,9 @@ Rails::Initializer.run do |config|
   config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'RedCloth'
-  # config.gem 'RMagick'
+  config.gem 'haml'
   config.gem 'htmlentities'
-  config.gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate', 
-      :source => 'http://gems.github.com'
+  config.gem 'will_paginate'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
--- db/migrate/20091110184154_make_article_position_unique.rb
+++ db/migrate/20091110184154_make_article_position_unique.rb
@@ -2,7 +2,7 @@ class MakeArticlePositionUnique < ActiveRecord::Migration
   def self.up
     cats = Category.all(:order => 'position ASC, archived ASC')
     1.upto(cats.size) do |i|
-        cats[i-1].position=i
+        cats[i-1].update_attribute('position', i)
     end
     change_column :categories, :position, :integer, :null => true
     add_index(:categories, [:archived, :position], :unique => true)
