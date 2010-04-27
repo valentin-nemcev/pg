2a7f1421f6f1b477766d5fec85478cbf6af43b7d
Валентин Немцев <zlob.o.zlob@gmail.com>
2010-04-28 02:20:51 +0400

    Доработки

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -3,24 +3,26 @@ module SiteHelper
     content_for :title, title + " &mdash; "
   end
   
+  
   def tag_cloud(font_size_range)
-    tags = Tag.all(:select => 'count(articles.id) as articles_count, tags.*', 
+    tags = Tag.all(:select => 'count(articles.id) as articles_cnt, tags.*', 
       :joins => :articles, 
-      :having => 'count(articles.id) > 1', 
+      :having => 'articles_cnt > 3', 
       :group => 'tags.id', 
       :order => 'name' )
 
-    compare_by_articles_count = lambda { |a, b| a.articles_count <=> b.articles_count }
-    maxlog = Math.log(tags.max(&compare_by_articles_count).articles_count || 1)
-    minlog = Math.log(tags.min(&compare_by_articles_count).articles_count || 1)
+    compare_by_articles_count = lambda { |a, b| a.articles_cnt.to_i <=> b.articles_cnt.to_i }
+    maxlog = Math.log(tags.max(&compare_by_articles_count).articles_cnt)
+    minlog = Math.log(tags.min(&compare_by_articles_count).articles_cnt)
     rangelog = maxlog - minlog;
     rangelog = 1 if maxlog==minlog
 
 
     tags.each { |t|
-      font_size = font_size_range.first + (font_size_range.last - font_size_range.first) * (Math.log(t.articles_count.to_i) - minlog) / rangelog 
+      font_size = font_size_range.first + (font_size_range.last - font_size_range.first) * (Math.log(t.articles_cnt) - minlog) / rangelog 
       yield t, sprintf("%.2f", font_size)
     }
   end
+  module_function :tag_cloud
   
 end
--- sandbox.rb
+++ sandbox.rb
@@ -12,23 +12,10 @@
 # order = "field(id, #{ids.join(',')})"
 # puts Article.all(:conditions => {:id => ids}, :order => order).map(&:id)
 
-def tag_cloud(max_size)
-  tags = Tag.all(:select => 'count(articles.id) as articles_count, articles.*', :joins => :articles, :having => 'count(articles.id) > 0', :group => 'tags.id', :order => 'count(articles.id) desc' )
-  
-  maxlog = Math.log(tags.first.articles_count)
-  minlog = Math.log(tags.last.articles_count)
-  rangelog = maxlog - minlog;
-  rangelog = 1 if maxlog==minlog
-  
-  
-  tags.each { |t|
-    font_size = 1 + (max_size - 1) * (Math.log(t.articles_count.to_i) - minlog) / rangelog 
-    yield t, font_size
-  }
-end
 
 
 
-tag_cloud tags, 5 do |tag, size| 
-  p tag, size
+
+SiteHelper.tag_cloud 0.5..1.5 do |tag, size| 
+  p "#{tag.uri} - #{size}"
 end
